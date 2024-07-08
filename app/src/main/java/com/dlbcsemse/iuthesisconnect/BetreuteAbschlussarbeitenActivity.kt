5fdae67ec3d94c509135bbf62b56814c8ddc2cf9
Marc Hirsch <marc-andre-hirsch@web.de>
2024-07-08 16:41:57 +0200

    <==Diese Version läuft ohne Probleme==>
    Weiterleitung von DashboardActivity zu MyThesisActivity wurde erstellt;
    ListView aus activity_dashboard.xml wurde zu einem Recycler ummodelliert um die erzeugten Objekte als ganzes anklicken zu können;
    dashboard_item_layout.xml wurde ummodelliert, damit diese geeignet als Button funktionieren können;
    activity_my_thesis.xml, DashboardButtonAdapter und MyThesisActivity wurden erstellt;
    
    DashboardItemAdapter muss in 43 bereinigt werden;
    activity_dashboard.xml muss in 40 bereinigt werden;
    dashboard_item_layout.xml muss in 69 bereinigt werden;
    DashboardButtonAdapter.kt muss in 32 bereinigt werden;
    
    MyThesisActivity Muss weiter ausgebaut werden;

---
+++ app/src/main/java/com/dlbcsemse/iuthesisconnect/BetreuteAbschlussarbeitenActivity.kt
@@ -0,0 +1,20 @@
+package com.dlbcsemse.iuthesisconnect
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BetreuteAbschlussarbeitenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_betreute_abschlussarbeiten)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
