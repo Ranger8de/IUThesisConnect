9cef2628fbc78363c549aba4c401be348fc82b51
Marc Hirsch <marc-andre-hirsch@web.de>
2024-07-09 15:16:26 +0200

    funktion ButtonAdapter umbenannt in DashboardButtonAdapter.kt (Funktionsname entspricht jetzt Klassenname; Die MyThesisActivity und Layout fertiggestellt - Datenfüllung mittels Datenbank fehlend; BetreuerboardActivity angefangen, es fehlt auch hier Datenbankanbindung sowie die Anzeigefunktion der betreuerboard_item_layout.xml;

--- app/src/main/java/com/dlbcsemse/iuthesisconnect/MyThesisActivity.kt
+++ app/src/main/java/com/dlbcsemse/iuthesisconnect/MyThesisActivity.kt
@@ -1,20 +1,50 @@
 package com.dlbcsemse.iuthesisconnect
 
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MyThesisActivity : AppCompatActivity() {
+    private lateinit var editTitle: EditText
+    private lateinit var editsupervisor: EditText
+    private lateinit var buttonSave: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_my_thesis)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        editTitle = findViewById(R.id.titelMyThesiseditTextData)
+        editsupervisor = findViewById(R.id.supervisorMyThesiseditTextData)
+        buttonSave = findViewById(R.id.myThesisbuttonSave)
+
+        buttonSave.setOnClickListener {
+            saveData()
         }
+
+        // Laden gespeicherter Daten beim Start der Activity
+        loadData()
+    }
+
+    private fun saveData() {
+        val title = editTitle.text.toString()
+        val supervisor = editsupervisor.text.toString()
+        val sharedSettings = getSharedPreferences("MyThesisSettings", Context.MODE_PRIVATE)
+        val editor = sharedSettings.edit()
+        editor.putString("savedTitle", title)
+        editor.putString("savedSupervisor", supervisor)
+        editor.apply()
+        Toast.makeText(this, "Daten gespeichert", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun loadData() {
+        val sharedPreferences = getSharedPreferences("MyThesisSettings", Context.MODE_PRIVATE)
+        val savedTitle = sharedPreferences.getString("savedTitle", "")
+        val savedSupervisor = sharedPreferences.getString("savedSupervisor", "")
+        editTitle.setText(savedTitle)
+        editsupervisor.setText(savedSupervisor)
     }
 }
